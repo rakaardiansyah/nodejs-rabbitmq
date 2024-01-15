d0f6414ecc69b778f613dfe79f58e559ad43f3e0
rakaardiansyah <raka.ardiansyah23@gmail.com>
2024-01-16 00:13:37 +0700

    rabbitmq nodejs

---
+++ consumer.js
@@ -0,0 +1,16 @@
+import { connect } from "amqplib";
+
+const connection = await connect("amqp://guest:guest@localhost:5672/");
+const channel = await connection.createChannel();
+
+
+channel.consume("sms", function (message){
+    console.info(message.fields.routingKey);
+    console.info(message.content.toString());
+},{
+    noAck: true
+})
+
+
+// await channel.close();
+// await connection.close();
